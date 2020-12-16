21c938907489ee956ef74fc0004854300d30c684
Juanmalc01 <juanmalozanochacot@gmail.com>
2020-12-16 19:57:16 +0100

    Login/Register

---
+++ log-register/alta_ok.php
@@ -0,0 +1,56 @@
+<?php
+
+session_start();
+
+$logueado = 0;
+
+//Parámetros de conexión
+$servidor = "localhost";
+$usuario = "root";
+$contraseña = "usbw";
+$bd = "practica_1";
+
+$nick = $_POST["nick"];
+$pass = $_POST["pass"];
+
+$con = mysqli_connect($servidor, $usuario, $contraseña, $bd);
+if (!$con) {
+	die("Con se ha podido realizar la conexión: " . mysqli_connect_error() . "<br>");
+} else {
+	mysqli_set_charset($con, "utf8");
+	echo "Se ha conectado a la base de datos" . "<br>";
+}
+
+$instruccion = "select count(*) as cuantos from usuarios where nick = '$nick'";
+$resultado = mysqli_query($con, $instruccion);
+while ($fila = $resultado->fetch_assoc()) {
+	$numero = $fila["cuantos"];
+}
+if ($numero == 0) {
+	echo "El usuario no existe";
+} else {
+	$instruccion = "select pass as cuantos from usuarios where pass = '$pass'";
+	$resultado = mysqli_query($con, $instruccion);
+
+	while ($fila = $resultado->fetch_assoc()) {
+		$password = $fila["cuantos"];
+	}
+
+	/////////////////
+
+	if ((strcmp($password, $pass) !== 0) || $pass == "") {
+		echo "Contraseña incorrecta";
+	} else {
+		echo "Login OK";
+		$_SESSION["nick_logueado"] = $nick;
+?>
+
+		<a href="https://www.google.es">Acceder al menu</a>
+
+<?php
+
+
+		$logueado = 1;
+	}
+}
+?>
---
+++ register/register_ok.php
@@ -0,0 +1,66 @@
+<?php
+	
+header("Content-Type: text/html;charset=utf-8");
+if (isset($_POST["nick"]))
+{
+
+	//Parámetros de conexión
+	$servidor = "localhost";
+	$usuario = "root";
+	$contraseña = "usbw";
+	$bd = "practica_1";
+
+	$nick = $_POST["nick"];
+	$pass = $_POST["pass"];
+	$mail = $_POST["mail"];
+	$confirmpass = $_POST["confirmpass"];
+	
+
+	$con = mysqli_connect($servidor, $usuario, $contraseña, $bd);
+	
+	if (!$con)
+	{
+		die("No se ha podido realizar la corrección ERROR:" . mysqli_connect_error() . "<br>");
+	}
+	
+	else
+	{
+		mysqli_set_charset ($con, "utf8");
+		echo "Se ha conectado a la base de datos"."<br>";
+	}
+	//////////////////////////////////////
+	
+	//Inserción de datos
+	
+	//Primero compruebo si el nick existe
+	$instruccion = "select count(*) as cuantos from register where nick = '$nick'";
+	$res = mysqli_query($con, $instruccion);
+	$datos = mysqli_fetch_assoc($res);
+	
+	if ($datos['cuantos'] == 0)
+	{
+		$instruccion = "insert into register values ('$nick','$mail','$pass','$confirmpass')";
+		$res = mysqli_query($con, $instruccion);
+		if (!$res) 
+		{
+			die("No se ha pordido crear el usuario");
+		}
+		else
+		{
+			echo "Usuario creado";
+			//me lleva al login para que pruebe mi contraseña
+			echo "<script>alert('Usuario creado correctamente');</script>";
+			include_once("login.html");
+		}
+	}
+	else
+	{
+		echo "El nick $nick no está disponible";
+	}
+
+}
+else 
+{
+	echo ("ERROR: No se puede introducir un nick en blanco");
+}
+?>
